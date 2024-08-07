caff1e9b65809ea8706efca6e70315d64484d1eb
JuaniPardo <juani.pardo@icloud.com>
2024-08-07 02:01:48 -0300

    [FIX]: Cambiado el campo "edad" por "fechaNacimiento"

--- app/src/main/java/com/davinci/sge_luju/RegistroActivity.java
+++ app/src/main/java/com/davinci/sge_luju/RegistroActivity.java
@@ -1,6 +1,9 @@
 package com.davinci.sge_luju;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.view.View;
@@ -15,15 +18,21 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import com.google.firebase.Timestamp;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class RegistroActivity extends AppCompatActivity {
 
     private static final String CONFIRMATION_CODE = "1234";
-    private EditText etFirstName, etLastName, etAge, etEmail, etPassword, etConfirmPassword, etConfirmationCode;
+    private EditText etFirstName, etLastName, etBirthDate, etEmail, etPassword, etConfirmPassword, etConfirmationCode;
     private Button btnRegister, btnBackToMain;
     private ProgressBar progressBar;
 
@@ -42,7 +51,7 @@ public class RegistroActivity extends AppCompatActivity {
         // Obtengo las referencias a los EditTexts
         etFirstName = findViewById(R.id.firstName);
         etLastName = findViewById(R.id.lastName);
-        etAge = findViewById(R.id.age);
+        etBirthDate = findViewById(R.id.birthDate);
         etEmail = findViewById(R.id.email);
         etPassword = findViewById(R.id.password);
         etConfirmPassword = findViewById(R.id.confirmPassword);
@@ -50,6 +59,8 @@ public class RegistroActivity extends AppCompatActivity {
         btnRegister = findViewById(R.id.registerBtn);
         btnBackToMain = findViewById(R.id.backToMainBtn);
         progressBar = findViewById(R.id.progressBar);
+
+        etBirthDate.setOnClickListener(v -> showDatePickerDialog());
 
         // Configuro el botón de registro
         btnRegister.setOnClickListener(v -> {
@@ -62,6 +73,16 @@ public class RegistroActivity extends AppCompatActivity {
             // Redirigir a la página de inicio
             navigateToMain();
         });
+    }
+
+    private Date convertirFecha(String fecha) {
+        SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
+        try {
+            return sdf.parse(fecha);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return null;
+        }
     }
 
     private void handleRegistration() {
@@ -77,7 +98,7 @@ public class RegistroActivity extends AppCompatActivity {
         crearUsuario(
                 etFirstName.getText().toString().trim(),
                 etLastName.getText().toString().trim(),
-                etAge.getText().toString().trim(),
+                convertirFecha(etBirthDate.getText().toString().trim()),
                 etEmail.getText().toString().trim(),
                 etPassword.getText().toString().trim()
         );
@@ -110,7 +131,7 @@ public class RegistroActivity extends AppCompatActivity {
 
         isAllFilled &= validateField(etFirstName, "Complete su nombre");
         isAllFilled &= validateField(etLastName, "Complete su apellido");
-        isAllFilled &= validateField(etAge, "Complete su edad");
+        isAllFilled &= validateField(etBirthDate, "Complete su edad");
         isAllFilled &= validateField(etEmail, "Complete su correo electrónico");
         isAllFilled &= validateField(etPassword, "Complete su contraseña");
         isAllFilled &= validateField(etConfirmPassword,  "Complete su contraseña");
@@ -131,12 +152,12 @@ public class RegistroActivity extends AppCompatActivity {
         return true;
     }
 
-    private void crearUsuario(String name, String lastName, String age, String email, String password) {
+    private void crearUsuario(String name, String lastName, Date birthDate, String email, String password) {
         FirebaseFirestore db = FirebaseFirestore.getInstance();
         Map<String, Object> alumno = new HashMap<>();
         alumno.put("nombre", name);
         alumno.put("apellido", lastName);
-        alumno.put("edad", age);
+        alumno.put("fec_nacimiento", new Timestamp(birthDate));
         alumno.put("email", email);
         alumno.put("password", password);
 
@@ -161,5 +182,19 @@ public class RegistroActivity extends AppCompatActivity {
         Intent intent = new Intent(RegistroActivity.this, MainActivity.class);
         startActivity(intent);
         finish(); // Finalizar la actividad actual
+    }
+
+    private void showDatePickerDialog() {
+        final Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        DatePickerDialog datePickerDialog = new DatePickerDialog(this,
+                (view, selectedYear, selectedMonth, selectedDay) -> {
+                    String selectedDate = selectedDay + "/" + (selectedMonth + 1) + "/" + selectedYear;
+                    etBirthDate.setText(selectedDate);
+                }, year, month, day);
+        datePickerDialog.show();
     }
 }
